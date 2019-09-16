7815dc8379d31f5d8c7019444c373d35606b1d0a
Brandon Chan <brandonhofaichan@gmail.com>
2019-09-16 14:06:11 -0500

    added a class

---
+++ ExampleOfClasses/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ExampleOfClasses;
+
+namespace Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nintendo = new Product();
+
+            nintendo.Name = "Switch Lite";
+            nintendo.Price = 199.99m;
+
+            Console.WriteLine($"The new {nintendo.Name} is available at the cost of {nintendo.Price}.");
+        }
+    }
+}
